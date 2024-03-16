79e91c5d57162b3dd58b0e2dbe09fc41138136f1
khanhnam2k2 <danghanam2k2@gmail.com>
2024-03-17 02:34:04 +0700

    tạo mới giao diện 2 màn hình

---
+++ screens/FavourtiesScreen.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function FavourtiesScreen() {
+  return (
+    <View>
+      <Text>FavourtiesScreen</Text>
+    </View>
+  );
+}
